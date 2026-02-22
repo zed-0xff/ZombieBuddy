fa4acd438a38e555c1dbf31b3901f91939d27574
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-22 17:31:57 +0200

    hide dirs only if experimental

--- java/src/me/zed_0xff/zombie_buddy/DirHider.java
+++ java/src/me/zed_0xff/zombie_buddy/DirHider.java
@@ -8,7 +8,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class DirHider {
-    private static boolean g_enabled = false;
+    private static boolean g_enabled = true;
 
     public static void setEnabled(boolean enabled) {
         g_enabled = enabled;
